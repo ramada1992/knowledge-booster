7c617f2c73d793e8fbc52e22454354d489faac6a
Eduard Shustrov <eshustrov@gmail.com>
2019-03-15 23:19:55 +0100

    stub

---
+++ src/main/java/com/gmail/dombrovski/aleksandr/booster/knowledge/KnowledgeBooster.java
@@ -0,0 +1,7 @@
+package com.gmail.dombrovski.aleksandr.booster.knowledge;
+
+public class KnowledgeBooster {
+    public static void main(String[] argv) {
+        System.out.println("Hello world");
+    }
+}
