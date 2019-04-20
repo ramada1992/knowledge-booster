55e8b09356cb09bada7f09169daf6b84ac69c02a
Aleksandr Dombrovski <aleksandr.dombrovski@gmail.com>
2019-04-20 16:01:00 +0300

    m

---
+++ src/main/java/com/gmail/dombrovski/aleksandr/booster/knowledge/GetMetaData.java
@@ -0,0 +1,7 @@
+package com.gmail.dombrovski.aleksandr.booster.knowledge;
+
+public class GetMetaData {
+    public static String getMetaData(String x){
+        return x;
+    }
+}
--- src/main/java/com/gmail/dombrovski/aleksandr/booster/knowledge/KnowledgeBooster.java
+++ src/main/java/com/gmail/dombrovski/aleksandr/booster/knowledge/KnowledgeBooster.java
@@ -1,7 +1,18 @@
 package com.gmail.dombrovski.aleksandr.booster.knowledge;
 
+import java.util.*;
+import java.util.stream.*;
+
 public class KnowledgeBooster {
     public static void main(String[] argv) {
-        System.out.println("Hello world");
+        System.out.println("Please enter file list in the following format FileName;FileName;FileName");
+        Scanner input = new Scanner(System.in);
+        String fileName = input.nextLine();
+        String[] inputString;
+        String separator = ";";
+        inputString = fileName.split(separator);
+        Stream<String> stream = Arrays.stream(inputString);
+        stream.forEach(x -> System.out.println(GetMetaData.getMetaData(x)));
+
     }
 }
