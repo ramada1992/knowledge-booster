a66010f476323363184cc4a8976305eca807fc64
Aleksandr Dombrovski <aleksandr.dombrovski@gmail.com>
2019-04-21 23:25:33 +0300

    seporator removed, fixed input values

--- src/main/java/com/gmail/dombrovski/aleksandr/booster/knowledge/KnowledgeBooster.java
+++ src/main/java/com/gmail/dombrovski/aleksandr/booster/knowledge/KnowledgeBooster.java
@@ -6,13 +6,10 @@ import java.util.stream.*;
 public class KnowledgeBooster {
     public static void main(String[] argv) {
 
-        if(argv.length == 0){
+        if(argv == null){
             System.out.println("No files");
         } else {
-            String separator = ";";
-            String inputString = argv[0];
-            String[] inputString2 = inputString.split(separator);
-            Stream<String> stream = Arrays.stream(inputString2);
+            Stream<String> stream = Arrays.stream(argv);
             stream.forEach(x -> System.out.println(GetMetaData.getMetaData(x)));
         }
 
