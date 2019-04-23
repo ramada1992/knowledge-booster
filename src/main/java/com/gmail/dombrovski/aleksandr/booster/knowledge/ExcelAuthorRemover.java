94a3d7fd6491c813615bd47f798dd3d33a24a9fc
Aleksandr Dombrovski <aleksandr.dombrovski@gmail.com>
2019-04-24 02:00:42 +0300

    added reading xls files

--- src/main/java/com/gmail/dombrovski/aleksandr/booster/knowledge/ExcelAuthorRemover.java
+++ src/main/java/com/gmail/dombrovski/aleksandr/booster/knowledge/ExcelAuthorRemover.java
@@ -1,15 +1,26 @@
 package com.gmail.dombrovski.aleksandr.booster.knowledge;
 
-import java.util.*;
-import java.util.stream.*;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
 
 public class ExcelAuthorRemover {
-    public static void main(String[] argv) {
-            Stream<String> stream = Arrays.stream(argv);
-            stream.forEach(x -> removeAuthors(x));
+    public static void main(String[] fileName) {
+        Arrays.stream(fileName).forEach(excelFile -> removeAuthors(excelFile));
     }
 
-    private static void removeAuthors(String fileName){
-        System.out.println("Remove authors name from "+ fileName );
+    private static void removeAuthors(String fileName) {
+        System.out.println("Removing authors name from " + fileName);
+        try {
+            FileInputStream excelFile = new FileInputStream(new File("src/main/xls/" + fileName));
+            Workbook workbook = new HSSFWorkbook(excelFile);
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        }
     }
 }
