742ea87c78f32783720729ff8e3ede913ca97354
Aleksandr Dombrovski <aleksandr.dombrovski@gmail.com>
2019-04-25 00:52:11 +0300

    build get data based on workbook

--- src/main/java/com/gmail/dombrovski/aleksandr/booster/knowledge/ExcelAuthorRemover.java
+++ src/main/java/com/gmail/dombrovski/aleksandr/booster/knowledge/ExcelAuthorRemover.java
@@ -1,12 +1,7 @@
 package com.gmail.dombrovski.aleksandr.booster.knowledge;
 
-import org.apache.poi.hpsf.NoPropertySetStreamException;
-import org.apache.poi.hpsf.PropertySet;
 import org.apache.poi.hpsf.SummaryInformation;
-import org.apache.poi.hpsf.UnexpectedPropertySetTypeException;
-import org.apache.poi.poifs.filesystem.DirectoryEntry;
-import org.apache.poi.poifs.filesystem.DocumentEntry;
-import org.apache.poi.poifs.filesystem.DocumentInputStream;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
 import java.io.FileInputStream;
@@ -20,18 +15,16 @@ public class ExcelAuthorRemover {
     }
 
     private static void removeAuthors(final String fileName) {
-        try (FileInputStream stream = new FileInputStream("src/main/xls/" + fileName)) {
+        try (FileInputStream fileStream = new FileInputStream("src/main/xls/" + fileName)) {
 
             System.out.println("Remove authors name from " + fileName);
 
-            DirectoryEntry dir = new POIFSFileSystem(stream).getRoot();
-            DocumentEntry siEntry = (DocumentEntry) dir.getEntry(SummaryInformation.DEFAULT_STREAM_NAME);
-            PropertySet ps = new PropertySet(new DocumentInputStream(siEntry));
-            String author = new SummaryInformation(ps).getAuthor();
+            final HSSFWorkbook workbook = new HSSFWorkbook(new POIFSFileSystem(fileStream));
+            SummaryInformation summaryInfo = workbook.getSummaryInformation();
 
-            System.out.println("File author " + author);
+            System.out.println("File author " + summaryInfo.getAuthor());
 
-        } catch (IOException | NoPropertySetStreamException | UnexpectedPropertySetTypeException ex) {
+        } catch (IOException ex) {
             ex.getStackTrace();
         }
     }
