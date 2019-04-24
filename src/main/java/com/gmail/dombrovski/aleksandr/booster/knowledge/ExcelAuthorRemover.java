39ba9d7a36309e806d1e1bba2233ce344dba0eb3
Aleksandr Dombrovski <aleksandr.dombrovski@gmail.com>
2019-04-24 04:18:49 +0300

    get document author name  functionality  added

--- src/main/java/com/gmail/dombrovski/aleksandr/booster/knowledge/ExcelAuthorRemover.java
+++ src/main/java/com/gmail/dombrovski/aleksandr/booster/knowledge/ExcelAuthorRemover.java
@@ -1,9 +1,14 @@
 package com.gmail.dombrovski.aleksandr.booster.knowledge;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.hpsf.NoPropertySetStreamException;
+import org.apache.poi.hpsf.PropertySet;
+import org.apache.poi.hpsf.SummaryInformation;
+import org.apache.poi.hpsf.UnexpectedPropertySetTypeException;
+import org.apache.poi.poifs.filesystem.DirectoryEntry;
+import org.apache.poi.poifs.filesystem.DocumentEntry;
+import org.apache.poi.poifs.filesystem.DocumentInputStream;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
@@ -17,10 +22,20 @@ public class ExcelAuthorRemover {
     private static void removeAuthors(String fileName) {
         System.out.println("Removing authors name from " + fileName);
         try {
-            FileInputStream excelFile = new FileInputStream(new File("src/main/xls/" + fileName));
-            Workbook workbook = new HSSFWorkbook(excelFile);
-        } catch (IOException e) {
-            System.err.format("IOException: %s%n", e);
+
+            FileInputStream stream = new FileInputStream("./src/main/xls/" + fileName);
+            POIFSFileSystem poifs = new POIFSFileSystem(stream);
+            DirectoryEntry dir = poifs.getRoot();
+            DocumentEntry siEntry = (DocumentEntry) dir.getEntry(SummaryInformation.DEFAULT_STREAM_NAME);
+            DocumentInputStream dis = new DocumentInputStream(siEntry);
+            PropertySet ps = new PropertySet(dis);
+            SummaryInformation si = new SummaryInformation(ps);
+            String author = si.getAuthor();
+            System.out.println(author);
+
+
+        } catch (IOException | NoPropertySetStreamException | UnexpectedPropertySetTypeException ex) {
+            ex.getStackTrace();
         }
     }
 }
