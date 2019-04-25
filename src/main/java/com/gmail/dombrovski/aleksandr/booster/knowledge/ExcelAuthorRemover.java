11c937650633380f1f08ef3d9f1096fca5161222
Aleksandr Dombrovski <aleksandr.dombrovski@gmail.com>
2019-04-25 05:07:39 +0300

    write chagnes to the file added

--- src/main/java/com/gmail/dombrovski/aleksandr/booster/knowledge/ExcelAuthorRemover.java
+++ src/main/java/com/gmail/dombrovski/aleksandr/booster/knowledge/ExcelAuthorRemover.java
@@ -5,6 +5,7 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -15,17 +16,29 @@ public class ExcelAuthorRemover {
     }
 
     private static void removeAuthors(final String fileName) {
-        try (FileInputStream fileStream = new FileInputStream("src/main/xls/" + fileName)) {
+        final String inputFile = ("src/main/xls/" + fileName);
+
+        try (final FileInputStream inputFileRead = new FileInputStream(inputFile)) {
 
             System.out.println("Remove authors name from " + fileName);
 
-            final HSSFWorkbook workbook = new HSSFWorkbook(new POIFSFileSystem(fileStream));
+            final HSSFWorkbook workbook = new HSSFWorkbook(new POIFSFileSystem(inputFileRead));
             SummaryInformation summaryInfo = workbook.getSummaryInformation();
+            String author = summaryInfo.getAuthor();
 
-            System.out.println("File author " + summaryInfo.getAuthor());
+            if (author != null) {
+                System.out.println("Current author name " + author);
 
+                inputFileRead.close();
+                summaryInfo.setAuthor(null);
+                final FileOutputStream outputFileWrite = new FileOutputStream(inputFile);
+                workbook.write(outputFileWrite);
+                outputFileWrite.close();
+            }
         } catch (IOException ex) {
             ex.getStackTrace();
         }
     }
 }
+
+
